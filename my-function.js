18930e795a93e3c1519e047eb6a9b44cd09ccc83
Mac <mac@Akshays-Air.local>
2020-10-16 21:35:59 -0400

    added keyword as query parameter in AWS lambda

--- my-function.js
+++ my-function.js
@@ -4,16 +4,19 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/say/:key',(req,res)=>{
-
-    axios.get('https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say')
-    .then(function (response) {
-        let res1=response.data+" "+req.query.key;
-        res.send(res1);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+app.get('/say', async (req,res)=>{
+    if(req.query && req.query.keyword){
+        await axios.get('https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword='+req.query.keyword)
+        .then(function (response) {
+            res.send(res1);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }else {
+        res.send("Please add any keyword");
+    }
+    
 })
 
 app.listen(port, () => {
