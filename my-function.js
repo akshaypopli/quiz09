b713772764a3462962b006358fae625bb3490db0
Mac <mac@Akshays-Air.local>
2020-10-16 22:04:10 -0400

    correction in file names, added lambda code file

--- my-function.js
+++ my-function.js
@@ -1,25 +1,19 @@
-const { json } = require('body-parser');
-const express = require('express')
-const axios = require('axios');
-const app = express();
-const port = 3000;
-
-app.get('/say', async (req,res)=>{
-    if(req.query && req.query.keyword){
-        //https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword=hi
-        await axios.get('https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword='+req.query.keyword)
-        .then(function (response) {
-            res.send(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-    }else {
-        res.send("Please add any keyword");
+exports.handler = async (event) => {
+    // TODO implement
+    if(event.httpMethod === 'GET') {
+        
+        if(event && event.queryStringParameters && event.queryStringParameters.keyword){
+            let keyword = event.queryStringParameters.keyword
+            let res = "Akshay Popli says " + keyword
+            return {
+                statusCode :200,
+                body: JSON.stringify(res)
+            }
+        }else {
+            return {
+                 statusCode:400,
+                 body:JSON.stringify({error:"Keyword is missing, Please add keyword"})
+            }
+        }
     }
-    
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const { json } = require('body-parser');
+const express = require('express')
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/say', async (req,res)=>{
+    if(req.query && req.query.keyword){
+        //https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword=hi
+        await axios.get('https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword='+req.query.keyword)
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }else {
+        res.send("Please add any keyword");
+    }
+    
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
