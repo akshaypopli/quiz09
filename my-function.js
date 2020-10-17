39c9e2bc2fb665e50df568962c287484f4d042d0
Mac <mac@Akshays-Air.local>
2020-10-16 21:47:34 -0400

    fixed parsing

--- my-function.js
+++ my-function.js
@@ -6,9 +6,10 @@ const port = 3000;
 
 app.get('/say', async (req,res)=>{
     if(req.query && req.query.keyword){
+        //https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword=hi
         await axios.get('https://hhq4a8aycd.execute-api.us-east-2.amazonaws.com/newLive/say?keyword='+req.query.keyword)
         .then(function (response) {
-            res.send(res1);
+            res.send(response.data);
         })
         .catch(function (error) {
             console.log(error);
